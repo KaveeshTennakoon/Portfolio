69cda1b3d1d0719209930bbd511915fd7583f62c
Kaveesh Tennakoon <kaveeshoffical@gmail.com>
2025-04-13 00:07:25 +0530

    removed the experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -54,6 +54,8 @@ export const technologies = [
   { name: "git", icon: git },
 ];
 
+//for us in experience section with sample data
+/*
 export const experiences = [
   {
     title: "AI/ML Intern",
@@ -66,31 +68,8 @@ export const experiences = [
       "also gaining a solid foundation in Machine Learning, covering topics like algorithms, data analysis, and model building.",
     ],
   },
-  {
-    title: "Mathwork Ai Virtual Intern",
-    company_name: "Mathwork | AICTE",
-    icon: mathwork,
-    iconBg: "#161329",
-    date: "May 2023 - Sep 2023",
-    points: [
-      "Completed virtual internship, gaining a strong foundation in MATLAB, including data analysis and processing.",
-      "Acquired practical skills in image and signal processing, including segmentation, batch processing, and spectral analysis.",
-      "Developed expertise in machine learning models for clustering, classification, and regression, and customized deep learning techniques for image classification.",
-    ],
-  },
-  {
-    title: "Artificial Intelligence Intern",
-    company_name: "Edunet Foundation | IBM SkillsBuild | AICTE",
-    icon: edunet,
-    iconBg: "#161329",
-    date: "June 2023 - July 2023",
-    points: [
-      "Engineered a comprehensive Mental Health Fitness Tracker ML model utilizing Python and scikit-learn.",
-      "Maximized the model's performance by refining model parameters and employing ensemble methods, yielding an outstanding accuracy percentage of 98.50%.",
-      "Leveraged 12 regression algorithms to attain precise outcomes in analyzing and predicting mental fitness levels across 150+ countries.",
-    ],
-  },
 ];
+*/
 
 export const projects = [
   {
